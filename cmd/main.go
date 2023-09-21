0e008069b31f79fbed8aea3d949d1b82aca83459
Sourabh Mehta <sourabh.mehta@arm.com>
2023-09-21 17:07:32 +0200

    added dummy

--- cmd/main.go
+++ cmd/main.go
@@ -8,14 +8,31 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/spf13/cobra"
 )
+
+var rootCmd = &cobra.Command{
+	Use:   "stringer",
+	Short: "stringer - a simple CLI to transform and inspect strings",
+	Long: `stringer is a super fancy CLI (kidding)
+   
+One can use stringer to modify or inspect strings straight from the terminal`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
 
 func main() {
 	uuidWithHyphen, _ := uuid.NewRandom()
 	uuid := strings.Replace(uuidWithHyphen.String(), "-", "", -1)
 	fmt.Println(uuid)
 	fmt.Println("Hello, World!")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
